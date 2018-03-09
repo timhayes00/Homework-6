20328a3574acf97d1476702a3d90f62383edb1b1
timhayes00 <timhayes00@gmail.com>
2018-03-08 23:59:07 -0500

    animation stop is now broken, but I'm fairly close

--- code.js
+++ code.js
@@ -1,14 +1,14 @@
 // Initial array of gifs
-var gifArray = ["puppers", "doggos", "honey badgers", "army ants", "caterpillars"];
+var topics = ["puppers", "doggos", "honey badgers", "army ants", "caterpillars", "sterling archer phrasing"];
 
 function renderButtons() {
   $("#buttons-view").empty();
-  for (var i = 0; i < gifArray.length; i++) {
+  for (var i = 0; i < topics.length; i++) {
     //dynamicaly generating buttons for each gif in the array
     var a = $("<button>");
     a.addClass("gif");
-    a.attr("data-name", gifArray[i]);
-    a.text(gifArray[i]);
+    a.attr("data-name", topics[i]);
+    a.text(topics[i]);
     $("#buttons-view").append(a);
   }
   console.log("render buttons finishing");
@@ -32,7 +32,7 @@ $("#buttons-view").on("click", "button", function() {
     })
       .then(function(response) {
         var results = response.data;
-
+console.log(response);
         for (var i = 0; i < results.length; i++) {
           var gifDiv = $("<div class='item'>");
 
@@ -42,6 +42,11 @@ $("#buttons-view").on("click", "button", function() {
           var gifUrl = $("<img>");
           
           gifUrl.attr("src", results[i].images.fixed_height_small.url);
+          gifUrl.attr("data-still", results[i].images.fixed_height_small_still.url)
+          gifUrl.attr("data-animate", results[i].images.fixed_height_small.url);
+          gifUrl.attr("data-state", "still");
+          gifUrl.attr("class", "gif");
+          console.log("gifUrl =" + gifUrl)
           gifDiv.prepend(p);
           gifDiv.prepend(gifUrl);
           $("#gifs-appear-here").prepend(gifDiv);
@@ -50,26 +55,20 @@ $("#buttons-view").on("click", "button", function() {
   });
 
   $("#add-gif").on("click", function(event) {
-    console.log("gifarray =" + gifArray);
     event.preventDefault();
-  
     var gif = $("#gif-input").val().trim();
-  
-    gifArray.push(gif);
-  
-    console.log("gifarray =" + gifArray);
+    topics.push(gif);
     renderButtons();
-    console.log("after calling render buttons");
   });
 
-//   var imageUrl = response.data.image_original_url;
-
-//   // Creating and storing an image tag
-//   var catImage = $("<img>");
-
-//   // Setting the catImage src attribute to imageUrl
-//   catImage.attr("src", imageUrl);
-//   catImage.attr("alt", "cat image");
-
-//   // Prepending the catImage to the images div
-//   $("#images").prepend(catImage);
+  $(".gif").on("click", function() {
+    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+    var state = $(this).attr("data-state");
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+  });
